691ae31162625c97609d937c92ff56278231bba5
rvwsolutions <info@rvwsolutions.com>
2025-07-06 20:08:01 +0530

    Fix duplicate useTranslation import

--- src/components/RoomsModule.tsx
+++ src/components/RoomsModule.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useTranslation } from 'react-i18next';
 import { useHotel } from '../context/HotelContext';
 import { useCurrency } from '../context/CurrencyContext';
 import { useAuth } from '../context/AuthContext';
